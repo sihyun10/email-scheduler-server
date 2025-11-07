5d2d4011828b674892174afe2fc1b67711f1e182
sihyun10 <hyhy200170@gmail.com>
2025-11-07 17:02:12 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/email/scheduler/email_scheduler_server/EmailSchedulerServerApplication.java
@@ -0,0 +1,13 @@
+package com.email.scheduler.email_scheduler_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedulerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchedulerServerApplication.class, args);
+	}
+
+}
