b6d8f7327940c24b31a0ae00eb43c29ccc9209eb
sihyun10 <hyhy200170@gmail.com>
2025-11-08 23:43:37 +0900

    feat: 스케줄러 클래스 생성
    스케줄링 작업할 클래스를 생성하였다.
    스프링이 자동으로 스케줄러로 등록할 수 있게 컴포넌트로 등록시켰다.
    일단, 10초마다 실행이 잘 되는지 로그를 통해 확인해보았다.

---
+++ src/main/java/com/email/scheduler/email_scheduler_server/MessageScheduler.java
@@ -0,0 +1,15 @@
+package com.email.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MessageScheduler {
+
+    @Scheduled(fixedRate = 10000)
+    public void testScheduler() {
+        log.info("[테스트] 스케줄러가 10초마다 실행됩니다.");
+    }
+}
