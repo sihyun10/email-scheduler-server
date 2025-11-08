7c61851866bc830446fbac03e9440aa0a7ef46eb
sihyun10 <hyhy200170@gmail.com>
2025-11-08 23:22:27 +0900

    feat: @EnableScheduling 선언
    스케줄러를 등록하려면 스케줄링 기능을 켜두어야 한다.
    따라서 @EnableScheduling를 선언하였다.
    선언하지 않으면 스케즐러 코드를 작성해도 실행이 되지 않는다.

--- src/main/java/com/email/scheduler/email_scheduler_server/EmailSchedulerServerApplication.java
+++ src/main/java/com/email/scheduler/email_scheduler_server/EmailSchedulerServerApplication.java
@@ -2,12 +2,13 @@ package com.email.scheduler.email_scheduler_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EmailSchedulerServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmailSchedulerServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSchedulerServerApplication.class, args);
+    }
 }
